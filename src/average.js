3a95f075e1e8a6baf26f6d96c8c487d010d816fc
Kaela Maselli <kmaselli@ucvts.org>
2019-11-19 10:50:08 -0500

    number 3 and some 4

--- src/average.js
+++ src/average.js
@@ -3,4 +3,14 @@ const readlineSync = require("readline-sync");
 const MAX = Number.MAX_SAFE_INTEGER;
 const MIN = 0;
 
-let nonNegativeInteger = Number(readlineSync.question("\nNon-negative integer: "));
+console.log()
+let nonNegativeInteger = 0
+
+do {
+  Number(readlineSync.question("Non-negative integer: "));
+}
+while (nonNegativeInteger !==0 || Number.isNaN(nonNegativeInteger) ||  !Number.isInteger(nonNegativeInteger) || nonNegativeInteger < MIN || nonNegativeInteger > MAX){
+    if (nonNegativeInteger === -1){
+      nonNegativeInteger += nonNegativeInteger
+    }
+}
--- src/odd.js
+++ src/odd.js
@@ -3,9 +3,20 @@ const readlineSync = require("readline-sync");
 const MAX = Number.MAX_SAFE_INTEGER;
 const MIN = Number.MIN_SAFE_INTEGER;
 
+let positiveInteger = 0
+console.log()
 while (positiveInteger<=0 || Number.isNaN(positiveInteger) ||  !Number.isInteger(positiveInteger) || positiveInteger < MIN || positiveInteger > MAX){
-  let positiveInteger = Number(readlineSync.question("\nPositive integer: "));
+  positiveInteger = Number(readlineSync.question("Positive integer: "));
 }
 
+let sum = 0;
 
-    }
+while (positiveInteger > 0) {
+  let digit = (positiveInteger % 10);
+  positiveInteger = Math.floor(positiveInteger / 10);
+
+  if (digit % 2 !== 0){
+    sum = sum + digit
+  }
+}
+console.log(`\n${sum}.`)
