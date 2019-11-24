c0816ecb4c8fd885caf7b91d718a1550961664f9
Kaela Maselli <ksmaselli@gmail.com>
2019-11-24 11:58:47 -0500

    mario?

--- src/mario.js
+++ src/mario.js
@@ -3,6 +3,28 @@ const readlineSync = require("readline-sync");
 const MAX = 24;
 const MIN = 1;
 
-let height = Number(readlineSync.question("\nHeight: "));
+let height = 0
+let space = "##";
 
-while 
+console.log("");
+
+do {
+      height = Number(readlineSync.question("Height: "));
+} while (height < 1 || height > 24 || Number.isNaN(height) || !Number.isInteger(height));
+
+let secondHeight = height;
+
+while (secondHeight > 1) {
+      space = " " + space
+    --secongHeight
+}
+
+console.log("\n" + space)
+
+while (height > 1) {
+    let firstSpace = space.replace(" #", "##")
+    console.log(firstSpace)
+    --height
+    let space = firstSpace
+}
+console.log()
