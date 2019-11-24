4c2ec46247b8ec290975f9c30c5c698ce126b088
Kaela Maselli <ksmaselli@gmail.com>
2019-11-24 12:05:52 -0500

    whywhwyhyw why

---
+++ src/luigi.js
@@ -0,0 +1,37 @@
+const readlineSync = require("readline-sync");
+
+let height = 0;
+let space = "##";
+let opposite = "##"
+
+console.log("");
+
+do {
+      height = Number(readlineSync.question("Height: "));
+} while (height < 1 || height > 24 || Number.isNaN(height) || !Number.isInteger(height));
+
+let secondHeight = height;
+let thirdHeight = height;
+
+while (secondHeight > 1) {
+      space = " " + space
+      --secondHeight
+}
+
+while (thirdHeight > 1) {
+      opposite = opposite + " "
+      --thirdHeight
+}
+
+console.log("\n" + space + "  " + opposite)
+
+while (height > 1) {
+    let firstSpace = space.replace(" #", "##")
+    let firstOpposite = opposite.replace("# ", "##")
+
+    console.log(firstSpace + "  " + firstOpposite)
+    --height
+
+    let space = firstSpace
+    let opposite = firstOpposite
+}
