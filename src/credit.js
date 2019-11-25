56ee902686dd5e49fd57bdff4e23ed6b03beb03e
Kaela Maselli <ksmaselli@gmail.com>
2019-11-25 05:50:19 -0500

    not finished

--- src/credit.js
+++ src/credit.js
@@ -1,1 +1,7 @@
-i literally do not get this
+const readlineSync = require("readline-sync");
+
+const number = 0
+
+while (number > 0) {
+  let digit = (number % 10);
+  number = Math.floor(number / 10);?????????????
