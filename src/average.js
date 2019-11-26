97551d825720c927a11de2fca5f1c79ecb27fb48
Kaela Maselli <ksmaselli@gmail.com>
2019-11-25 22:25:37 -0500

    Update average.js

--- src/average.js
+++ src/average.js
@@ -1,34 +1,20 @@
 const readlineSync = require("readline-sync");
 
-const MAX = Number.MAX_SAFE_INTEGER;
-const MIN = 0;
+let integer = 0;
+let value = 0;
+x = 0
 
-console.log()
+console.log("");
 
-let sum = 0;
-let count = 0;
-let done = false;
+while (value > Number.MAX_SAFE_INTEGER || !Number.isInteger(value) || value !== -1) {
+    value = Number(readlineSync.question("Non-negative integer: "));
 
-while (!done) {
-  const value = Number(readlineSync.question("Non-negative integer: "));
-
-  if (Number.isNaN(value) {
-    // ignore
-  } else if (!Number.isInteger(value)) {
-    // ignore
-  } else if (!Number.isSafeInteger(value)) {
-    // ignore
-  } else {
-    if (value < 0 && count === 0) {
-      // ignore
-    } else if (value < 0) {
-      done = true;
-    } else {
-      sum = sum + value;
-      count = count + 1;
+    if (value !== -1) {
+        integer = integer + value;
+        x = x + 1
     }
-  }
 }
-let average = (sum/count).toLocaleString("en", { minimumFractionDigits: 3, maximumFractionDigits: 3});
+let average = integer / x
+average = average.toLocaleString('en', {minimumFractionDigits: 3, maximumFractionDigits: 3});
 
-console.log("\n" + average + ".")
+console.log("\n" + average + "\n");
